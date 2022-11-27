f129e3340316a89338ca65ebb463baaec566e16b
Ruben Hernandez Alvarado <rubhernandez42@gmail.com>
2022-11-26 23:32:16 -0600

    Se agregaron documentos js para armar el canvas

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
 console.log(maps);
+
+const canvas = document.querySelector("#game");
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame)
+
+function startGame() {
+    
+
+    // game.fillRect(0, 100, 100, 100);
+    // game.clearRect(50,50,50,50);
+    // game.font = "25px Verdana"
+    // game.fillStyle = "purple";
+    // game.textAlign = "center";
+    // game.fillText('Ruben', 100, 50);
+}
