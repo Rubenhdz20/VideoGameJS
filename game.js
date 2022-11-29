b70c1137ccd2b273cb014cf0479d342948dbef76
Ruben Hernandez Alvarado <rubhernandez42@gmail.com>
2022-11-29 03:11:06 -0600

    Adding canvas size function and start game function

--- game.js
+++ game.js
@@ -1,17 +1,42 @@
-console.log(maps);
-
-const canvas = document.querySelector("#game");
+const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame)
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  
+  elementsSize = canvasSize / 10;
+
+  startGame();
+}
 
 function startGame() {
-    
+  console.log({ canvasSize, elementsSize });
 
-    // game.fillRect(0, 100, 100, 100);
-    // game.clearRect(50,50,50,50);
-    // game.font = "25px Verdana"
-    // game.fillStyle = "purple";
-    // game.textAlign = "center";
-    // game.fillText('Ruben', 100, 50);
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  }
 }
+
+// PROPIEDADES DE UN CANVAS 
+// game.fillRect(0, 100, 100, 100);
+// game.clearRect(50,50,50,50);
+// game.font = "25px Verdana"
+// game.fillStyle = "purple";
+// game.textAlign = "center";
+// game.fillText('Ruben', 100, 50);
