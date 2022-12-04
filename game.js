ab13d0578c0a07f51a2c1e97c509f6e4ba6b12aa
Ruben Hernandez Alvarado <rubhernandez42@gmail.com>
2022-12-04 03:31:37 -0600

    refactorizando con metodo ForEach cada item del canvas

--- game.js
+++ game.js
@@ -8,11 +8,9 @@ window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 
 function setCanvasSize() {
-  if (window.innerHeight > window.innerWidth) {
-    canvasSize = window.innerWidth * 0.8;
-  } else {
-    canvasSize = window.innerHeight * 0.8;
-  }
+  window.innerHeight > window.innerWidth
+  ? canvasSize = window.innerWidth * 0.7
+  : canvasSize = window.innerHeight * 0.7;
   
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
@@ -26,18 +24,28 @@ function startGame() {
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
 
-  const map = maps[1];
+  const map = maps[0];
   const mapRows = map.trim().split('\n');
   const mapRowsCols = mapRows.map(row => row.trim().split(''));
   console.log({maps, mapRows, mapRowsCols});
 
-  for (let row = 1; row <= 10; row++) {
-    for (let col = 1; col <= 10; col++) {
-      game.fillText(emojis[mapRowsCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
-    }
-  }
+  mapRowsCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
+      game.fillText(emoji, posX, posY);
+    }) 
+  })
+
+  // for (let row = 1; row <= 10; row++) {
+  //   for (let col = 1; col <= 10; col++) {
+  //     game.fillText(emojis[mapRowsCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
+  //   }
+  // }
   
 }
+
 
 // PROPIEDADES DE UN CANVAS 
 // game.fillRect(0, 100, 100, 100);
